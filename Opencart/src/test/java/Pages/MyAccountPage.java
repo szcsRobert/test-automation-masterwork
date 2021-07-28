697e211eb4b8dee1708f75ff43a105bf8472b7c1
szcsRobert <szcsrbrt@gmail.com>
2021-07-28 16:31:01 +0200

    MyAccountPage has been created

---
+++ Opencart/src/test/java/Pages/MyAccountPage.java
@@ -0,0 +1,32 @@
+package Pages;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyAccountPage extends BasePage {
+  private static final Logger LOG = LoggerFactory.getLogger(LoginPage.class);
+
+  @FindBy(xpath = "//*[@id=\"content\"]/ul[1]/li[1]/a")
+  WebElement editYourAccount;
+
+  public MyAccountPage() {
+  }
+
+  public MyAccountPage(WebDriver driver) {
+    super(driver);
+  }
+
+  @Step("My Account page should load successfully")
+  public boolean isLoaded() {
+    LOG.info("My Account page loaded success");
+    return driver.getCurrentUrl()
+        .equals("http://test-automation-shop2.greenfox.academy/index.php?route=account/account");
+  }
+}
